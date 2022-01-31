02be1f688a091f4e3d30894c2dc8c5460c2302d5
Alexander Kamkin <kamkin@ispras.ru>
2022-01-31 18:42:39 +0300

    AXI-Stream row-by-row adapter added.
    Signed-off-by: Alexander Kamkin <kamkin@ispras.ru>

--- src/chisel/IDCTAXIrow.scala
+++ src/chisel/IDCTAXIrow.scala
@@ -11,13 +11,13 @@ package ru.ispras.idct
 import chisel3._
 import chisel3.stage.ChiselStage
 
-// AXI-Stream adapter for element-by-element transmission.
-class IDCTAXI extends Module {
+// AXI-Stream adapter for row-by-row transmission.
+class IDCTAXIrow extends Module {
   val io = IO(new Bundle {
-    val i_tdata  = Input(SInt(Def.INPUT_WIDTH.W)) // Element F[u,v]
+    val i_tdata  = Input(UInt((8*Def.INPUT_WIDTH).W)) // Row F[u,*]
     val i_tvalid = Input(Bool())
     val i_tready = Input(Bool())
-    val o_tdata  = Output(SInt(Def.OUTPUT_WIDTH.W)) // Element f[i,j]
+    val o_tdata  = Output(UInt((8*Def.OUTPUT_WIDTH).W)) // Row f[i,*]
     val o_tvalid = Output(Bool())
     val o_tready = Output(Bool())
   })
@@ -25,8 +25,8 @@ class IDCTAXI extends Module {
   val iblk = Reg(Vec(8*8, SInt(Def.INPUT_WIDTH.W)))
   val oblk = Reg(Vec(8*8, SInt(Def.OUTPUT_WIDTH.W)))
 
-  val iidx = RegInit(0.U(6.W))
-  val oidx = RegInit(0.U(6.W))
+  val iidx = RegInit(0.U(3.W))
+  val oidx = RegInit(0.U(3.W))
 
   // Copy the output block.
   val copy = RegInit(false.B)
@@ -38,7 +38,7 @@ class IDCTAXI extends Module {
   // Combinational 8x8 IDCT core.
   val idct = Module(new IDCT)
 
-  copy := (iidx === 63.U)
+  copy := (iidx === 7.U)
 
   for (i <- 0 to 63) {
     idct.io.iblk(i) := iblk(i)
@@ -49,32 +49,40 @@ class IDCTAXI extends Module {
     }
   }
 
-  // Receive an input element.
+  // Receive an input row.
+  val mask = (1.U << Def.INPUT_WIDTH) - 1.U
   when (io.i_tvalid & ready) {
-    iblk(iidx) := io.i_tdata;
+    for (i <- 0 to 7) {
+      iblk((iidx << 3) + i.U) := ((io.i_tdata >> (i * Def.INPUT_WIDTH)) & mask).asSInt()
+    }
     iidx := iidx + 1.U
   }
 
   // Previous transmission has not been completed.
-  when (iidx === 62.U | ~ready) {
+  when (iidx === 6.U | ~ready) {
     ready := ~send
   }
 
   // Start/stop transmission.
-  when (copy | oidx === 63.U) {
+  when (copy | oidx === 7.U) {
     send := copy
   }
 
-  // Send an output element.
+  // Send an output row.
   when (send & io.i_tready) {
     oidx := oidx + 1.U
   }
 
-  io.o_tdata := oblk(oidx)
+  val tdata = Wire(Vec(8, SInt(Def.OUTPUT_WIDTH.W)))
+  for (i <- 0 to 7) {
+    tdata(i) := oblk((oidx << 3) + i.U)
+  }
+
+  io.o_tdata  := tdata.asUInt()
   io.o_tvalid := send
   io.o_tready := ready
 }
 
-object IDCTAXIDriver extends App {
-  (new ChiselStage).emitVerilog(new IDCTAXI)
+object IDCTAXIrowDriver extends App {
+  (new ChiselStage).emitVerilog(new IDCTAXIrow)
 }
